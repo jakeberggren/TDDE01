be920a439bd3c79ddd844f82b92617614e4c5f94
Jakob Berggren <jakbe841@student.liu.se>
2022-12-21 00:49:51 +0100

    completed lab1 assign1. Fixed issue in assign2. Refactored code

--- lab1/assignment_1.R
+++ lab1/assignment_1.R
@@ -1,9 +1,14 @@
 # Assigment 1 of Laboration 1 of course TDDE01,
 # Machine Learning at Linkoping University, Sweden
 
-###########
-# Task 1 - Import data and divide into train, validation and test (50%/25%/25%)
-###########
+
+########### Libraries #############
+library(kknn)
+########### Libraries #############
+
+#########################
+######## Task 1 #########
+#########################
 
 # Read data
 data <- read.csv("data/optdigits.csv", header = FALSE)
@@ -24,61 +29,51 @@ id3 <- setdiff(id1, id2)
 data_test <- data[id3, ]
 
 
-###########
-# Task 2 - Use training data to fit 30-nearest neighbor classifier and estimate
-# confusion matrices and misclassification rates for training and test data
-###########
+#########################
+######## Task 2 #########
+#########################
 
-data_train$X0.26 <- as.factor(data_train$X0.26)
-data_test$X0.26 <- as.factor(data_test$X0.26)
-library(kknn)
+data_train$V65 <- as.factor(data_train$V65)
+data_test$V65 <- as.factor(data_test$V65)
 # Using data to fit 30-nearest neighbor on train and test data
-model_train <- kknn(X0.26 ~ ., train = data_train, test = data_train, k = 30,
-                    kernel = "rectangular")
-model_test <- kknn(X0.26 ~ ., train = data_train, test = data_test, k = 30,
-                   kernel = "rectangular")
+model_train <- kknn(V65 ~ ., train = data_train, test = data_train, k = 30, kernel = "rectangular")
+model_test <- kknn(V65 ~ ., train = data_train, test = data_test, k = 30, kernel = "rectangular")
 
 prediction_train <- predict(model_train)
 prediction_test <- predict(model_test)
 
 # Confusion matrices
-cm_train <- table(prediction_train, data_train$X0.26)
-cm_test <- table(prediction_test, data_test$X0.26)
+cm_train <- table(prediction_train, data_train$V65)
+cm_test <- table(prediction_test, data_test$V65)
 
 # Function to compute misclassification rate
-misclass <- function(x, x1) {
+MisClass <- function(x, x1) {
   n <- length(x)
   return(1 - sum(diag(table(x, x1))) / n)
 }
 
 # Computing misclassification rates from confusion matrices
-misclass_train <- misclass(prediction_train, data_train$X0.26)
-misclass_test <- misclass(prediction_test, data_test$X0.26)
+misclass_train <- MisClass(prediction_train, data_train$V65)
+misclass_test  <- MisClass(prediction_test, data_test$V65)
 
 
-###########
-# Task 3 - Find cases of digit "8" which were easy/hard to classify.
-# Visualize with heatmap().
-###########
+#########################
+######## Task 3 #########
+#########################
 
 # Combining dataset of 8s in the training data and the probability of
 # being an 8 in the kknn prob table
-train8 <- data_train[data_train$X0.26 == "8", ]
-prob8 <- model_train[["prob"]][data_train$X0.26 == "8", ]
+train8 <- data_train[data_train$V65 == "8", ]
+prob8 <- model_train[["prob"]][data_train$V65 == "8", ]
 
 # Find index of the 3 hardest and the 2 easiest 8s to classify
-prob8_hardest <- sort(prob8[, "8"], decreasing = FALSE,
-                      index.return = TRUE)$ix[1:3]
-prob8_easiest <- sort(prob8[, "8"], decreasing = TRUE,
-                      index.return = TRUE)$ix[1:2]
+prob8_hardest <- sort(prob8[, "8"], decreasing = FALSE, index.return = TRUE)$ix[1:3]
+prob8_easiest <- sort(prob8[, "8"], decreasing = TRUE,  index.return = TRUE)$ix[1:2]
 
 # Remove last column and reshape into 8x8 numeric matrix
-prob8_hardest_1 <- matrix(as.numeric(train8[prob8_hardest[1], ][-65]),
-                          nrow = 8, ncol = 8)
-prob8_hardest_2 <- matrix(as.numeric(train8[prob8_hardest[2], ][-65]),
-                          nrow = 8, ncol = 8)
-prob8_hardest_3 <- matrix(as.numeric(train8[prob8_hardest[3], ][-65]),
-                          nrow = 8, ncol = 8)
+prob8_hardest_1 <- matrix(as.numeric(train8[prob8_hardest[1], ][-65]), nrow = 8, ncol = 8)
+prob8_hardest_2 <- matrix(as.numeric(train8[prob8_hardest[2], ][-65]), nrow = 8, ncol = 8)
+prob8_hardest_3 <- matrix(as.numeric(train8[prob8_hardest[3], ][-65]), nrow = 8, ncol = 8)
 
 # Visualize
 heatmap(t(prob8_hardest_1), Colv = "Rowv", Rowv = NA)
@@ -86,57 +81,61 @@ heatmap(t(prob8_hardest_2), Colv = "Rowv", Rowv = NA)
 heatmap(t(prob8_hardest_3), Colv = "Rowv", Rowv = NA)
 
 # Remove last column and reshape into 8x8 numeric matrix
-prob8_easiest_1 <- matrix(as.numeric(train8[prob8_easiest[1], ][-65]),
-                          nrow = 8, ncol = 8)
-prob8_easiest_2 <- matrix(as.numeric(train8[prob8_easiest[2], ][-65]),
-                          nrow = 8, ncol = 8)
+prob8_easiest_1 <- matrix(as.numeric(train8[prob8_easiest[1], ][-65]), nrow = 8, ncol = 8)
+prob8_easiest_2 <- matrix(as.numeric(train8[prob8_easiest[2], ][-65]), nrow = 8, ncol = 8)
 
 # Visualize
 heatmap(t(prob8_easiest_1), Colv = "Rowv", Rowv = NA)
 heatmap(t(prob8_easiest_2), Colv = "Rowv", Rowv = NA)
 
 
-###########
-# Task 4 - Fit K-nearest neighbor classifiers to training data for values
-# of K = 1-30 and plot the dependence of the training and validation
-# misclassification errors on the value on the value of K.
-###########
+#########################
+###### Task 4 & 5 #######
+#########################
 
-multi_misclass_train <- c()
-multi_misclass_valid <- c()
-for (x in 1:30) {
-  model_train_temp <- kknn(X0.26 ~ ., train = data_train, test = data_train,
-                           k = x, kernel = "rectangular")
-  prediction_train_temp <- predict(model_train_temp)
-  multi_misclass_train <- append(multi_misclass_train,
-    misclass(prediction_train_temp, data_train$X0.26))
-
-  model_valid_temp <- kknn(X0.26 ~ ., train = data_train, test = data_valid,
-                           k = x, kernel = "rectangular")
-  prediction_valid_temp <- predict(model_valid_temp)
-  multi_misclass_valid <- append(
-    multi_misclass_valid,
-    misclass(prediction_valid_temp, data_valid$X0.26))
+# Helper function for calculating cross entropy
+LogProb <- function(x) {
+  -log(x + 1e-15)
 }
 
+multi_misclass_train <- rep(0, 30)
+multi_misclass_valid <- rep(0, 30)
+cross_entropy <- rep(0, 30)
+for (x in 1:30) {
+  model_train_temp <- kknn(V65 ~ ., train = data_train, test = data_train, k = x, kernel = "rectangular")
+  model_valid_temp <- kknn(V65 ~ ., train = data_train, test = data_valid, k = x, kernel = "rectangular")
+  
+  prediction_train_temp <- predict(model_train_temp)
+  prediction_valid_temp <- predict(model_valid_temp)
+  
+  multi_misclass_train[x] <- MisClass(prediction_train_temp, data_train$V65)
+  multi_misclass_valid[x] <- MisClass(prediction_valid_temp, data_valid$V65)
+  
+  # Calculating Cross Entropy
+  for (y in 0:9) {
+    prob_cross_entropy <- model_valid_temp$prob[which(data_valid$V65 == y), y+1]
+    prob_cross_entropy <- sum(sapply(prob_cross_entropy, LogProb))
+    cross_entropy[x] <- cross_entropy[x] +  prob_cross_entropy 
+  }
+}
+
+# Finding best k according to misclassification
 best_k <- which.min(multi_misclass_valid)
-best_k_model <- kknn(X0.26 ~ ., train = data_train, test = data_test,
-                     k = best_k, kernel = "rectangular")
+best_k_model <- kknn(V65 ~ ., train = data_train, test = data_test, k = best_k, kernel = "rectangular")
 best_k_model_prediction <- best_k_model$fitted.values
-best_k_misclass <- misclass(best_k_model_prediction, data_test$X0.26)
+best_k_misclass <- MisClass(best_k_model_prediction, data_test$V65)
 
+# Plot of mislcassification rates
 plot(multi_misclass_train * 100, col = "blue", ylim = c(0, 6),
-     xlab = "K-nearest neighbors", ylab = "Misclassification rate in %",
-     pch = 20)
-points(multi_misclass_valid * 100, col = "green", pch = 20)
-points(7, best_k_misclass * 100, col = "red", pch = 20)
-mtext("Difference in misclassification
-      dependent on number of neighbours in KNN")
+     xlab = "K-nearest neighbors",
+     ylab = "Misclassification rate in %", pch = 5)
+
+points(multi_misclass_valid * 100, col = "green", pch = 5)
+points(7, best_k_misclass * 100, col = "red", pch = 5)
+mtext("Difference in misclassification dependent on number of neighbours in KNN")
 legend("topleft", c("misclass. training data", "misclass. validation data",
-                   "missclass. test data with optimal K value"),
-       fill = c("blue", "green", "red"))
+                    "missclass. test data with optimal K value"),
+       col = c("blue", "green", "red"), pch = 5)
 
-
-###########
-# Task 5
-###########
+plot(cross_entropy, col = "blue", type = "b", pch = 5, ylab = "Entropy", xlab = "K-Value")
+mtext("Error of validation data as cross entropy")
--- lab1/assignment_2.R
+++ lab1/assignment_2.R
@@ -1,10 +1,14 @@
 # Assignment 2 of Laboration 1 of course TDDE01,
 # Machine Learning at Linkoping University, Sweden
 
-###########
-# Task 1 - Divide data into training and test data (60/40)
-# and scale it appropriately
-###########
+########### Libraries #############
+library(caret)
+library(dplyr)
+########### Libraries #############
+
+#########################
+######## Task 1 #########
+#########################
 
 # read file
 data <- read.csv("data/parkinsons.csv")
@@ -17,20 +21,16 @@ data_train <- data[id, ]
 data_test <- data[-id, ]
 
 # Scale data using library caret
-library(caret)
 scaler <- preProcess(data_train)
 train_scaled <- predict(scaler, data_train)
 test_scaled <- predict(scaler, data_test)
 
-###########
-# Task 2 - Compute a linear regression model from the training data,
-# estimate training and test MSE (Mean Square Error)
-###########
+#########################
+######## Task 2 #########
+#########################
 
-model_train <- lm(motor_UPDRS ~ . - age - sex - subject. - test_time
-  - total_UPDRS, data = train_scaled)
-model_test <- lm(motor_UPDRS ~ . - age - sex - subject. - test_time
-  - total_UPDRS, data = test_scaled)
+model_train <- lm(motor_UPDRS ~ . - age - sex - subject. - test_time - total_UPDRS, data = train_scaled)
+model_test <- lm(motor_UPDRS ~ . - age - sex - subject. - test_time - total_UPDRS, data = test_scaled)
 
 summary(model_train)
 summary(model_test)
@@ -42,20 +42,17 @@ mse_train <- mean((train_scaled$motor_UPDRS - pred_train)^2)
 mse_test <- mean((test_scaled$motor_UPDRS - pred_test)^2)
 
 
-###########
-# Task 3 - Implement functions loglikelihood, ridge , ridgeOpt and DF using
-# using basic R commands only (no external packages allowed).
-###########
+#########################
+######## Task 3 #########
+#########################
 
-library(dplyr)
 # Extracting data needed for functions below
 n <- nrow(train_scaled)
 y <- as.matrix(train_scaled$motor_UPDRS)
 x <- as.matrix(train_scaled %>% select(Jitter...:PPE))
 
 Loglikelihood <- function(theta, sigma) {
-  return(-n / 2 * log(2 * pi * sigma^2) - (1 / (2 * sigma^2)) *
-           sum((y - x %*% theta)^2))
+  return(-n / 2 * log(2 * pi * sigma^2) - (1 / (2 * sigma^2)) * sum((y - x %*% theta)^2))
 }
 
 Ridge <- function(theta, lambda) {
@@ -70,16 +67,13 @@ RidgeOpt <- function(lambda) {
 
 I <- as.matrix(diag(16))
 DF <- function(lambda) {
-  return(sum(diag((x %*% (t(x) %*% x + lambda * I)^(-1)) %*% t(x))))
+  return(sum(diag((x %*% solve((t(x) %*% x + lambda * I))) %*% t(x))))
 }
 
 
-###########
-# Task 4 - Use RidgeOpt to compute optimal theta parameters for lambda = 1,
-# lambda = 100 and lambda = 1000. Use parameters to predict motor_UPDRS values
-# for training and test data and report the training and test MSE values.
-# Compute and compare degrees of freedom
-###########
+#########################
+######## Task 4 #########
+#########################
 
 theta_opt_1 <- RidgeOpt(lambda = 1)
 theta_opt_100 <- RidgeOpt(lambda = 100)
@@ -108,6 +102,6 @@ mse_train_opt_1000 <- mean((train_scaled$motor_UPDRS - pred_train_opt_1000)^2)
 mse_test_opt_1000  <- mean((test_scaled$motor_UPDRS  - pred_test_opt_1000)^2)
 
 # Degrees of freedom
-df_1 <- DF(lambda = 1) # 255.9955
-df_100 <- DF(lambda = 100) # 255.5585
-df_1000 <- DF(lambda = 1000) # 252.4633
+df_1 <- DF(lambda = 1) # 13.860736
+df_100 <- DF(lambda = 100) # 9.924871
+df_1000 <- DF(lambda = 1000) # 5.643925
